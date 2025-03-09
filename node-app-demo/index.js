72f17797ffae0d7e6c8bb3e2abdd20e04db63595
Excavator <kvronin@gmail.com>
2025-03-09 14:43:13 +0500

    add vase struct for node-app-demo

---
+++ node-app-demo/index.js
@@ -0,0 +1,56 @@
+const { ApolloServer } = require("apollo-server");
+const neo4j = require("neo4j-driver");
+const { Neo4jGraphQL } = require("@neo4j/graphql");
+
+// check if work > nc -zv localhost 7687
+const driver = neo4j.driver(
+  "bolt://localhost:7687",
+  neo4j.auth.basic("neo4j", "password")
+);
+
+const typeDefs = /* GraphQL */ `
+  type Business {
+    businessId: ID!
+    name: String!
+    city: String!
+    state: String!
+    address: String!
+    location: Point!
+    reviews: [Review!]! @relationship(type: "REVIEWS", direction: IN)
+    categories: [Category!]!
+    @relationship(type: "IN_CATEGORY", direction: OUT)
+  }
+
+  type User {
+    userID: ID!
+    name: String!
+    reviews: [Review!]! @relationship(type: "WROTE", direction: OUT)
+  }
+
+  type Review {
+    reviewId: ID!
+    stars: Float!
+    date: Date!
+    text: String
+    user: User! @relationship(type: "WROTE", direction: IN)
+    business: Business! @relationship(type: "REVIEWS", direction: OUT)
+  }
+
+  type Category {
+    name: String!
+    businesses: [Business!]! @relationship(type: "IN_CATEGORY", direction: IN)
+  }
+`;
+
+const neoSchema = new Neo4jGraphQL({ typeDefs, driver });
+
+neoSchema.getSchema().then((schema) => {
+  const server = new ApolloServer({
+    schema
+  });
+  server.listen().then(({url}) => {
+    console.log(`🚀  GraphQL server ready at ${url}`);
+  });
+});
+
+// node index.js
