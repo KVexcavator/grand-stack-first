0648d581f62446ca969a9126387d26364603b834
Excavator <kvronin@gmail.com>
2025-03-24 20:51:41 +0500

    add fragment and refetch data

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # grand-stack-first
+How app up:
+docker compose up
+http://localhost:7474
+neo4j password
+node node-app-demo/index.js
+cd web-react/
+npm run
--- web-react/src/App.js
+++ web-react/src/App.js
@@ -2,22 +2,45 @@ import React, { useState } from "react";
 import BusinessResults from "./BusinessResults";
 import { gql, useQuery } from "@apollo/client";
 
-const GET_BUSINESSES_QUERY = gql`
-  {
-    businesses {
-      businessId
+const BUSINESS_DETAILS_FRAGMENT = gql`
+  fragment businessDetails on Business {
+    businessId
+    name
+    address
+    categories {
       name
-      address
-      categories {
-        name
-      }
     }
   }
+`
+
+const GET_BUSINESSES_QUERY = gql`
+  query BusinessesByCategory($selectedCategory: String!) {
+    businesses (
+      where: { categories_SOME: { name_CONTAINS: $selectedCategory } }
+    ){
+      ...businessDetails
+    }
+  }
+
+  ${BUSINESS_DETAILS_FRAGMENT}
 `;  
   
 function App() {
-  const [selectedCategory, setSelectedCategory] = useState("All");
-  const { loading, error, data } = useQuery(GET_BUSINESSES_QUERY);
+  const [selectedCategory, setSelectedCategory] = useState("");
+
+  // обновление через интервал
+  // const { loading, error, data, refetch } = useQuery(GET_BUSINESSES_QUERY, {
+  //   variables: { selectedCategory },    
+  //   pollInterval: 500    
+  // });
+
+  // обновление путем повторной выборки
+  const { loading, error, data, refetch} = useQuery(
+    GET_BUSINESSES_QUERY,
+    {
+      variables: {selectedCategory},
+    }
+  )
 
   if (error) return <p>Error</p>;
   if (loading) return <p>Loading...</p>;
@@ -31,13 +54,16 @@ function App() {
             value={selectedCategory}
             onChange={(event) => setSelectedCategory(event.target.value)}
           >
-            <option value="All">All</option>
+            <option value="">All</option>
             <option value="Library">Library</option>
             <option value="Restaurant">Restaurant</option>
             <option value="Car Wash">Car Wash</option>
           </select>
         </label>
-        <input type="submit" value="Submit" />
+        {/* обновление через интервал */}
+        {/* <input type="submit" value="Submit" /> */}
+        {/* обновление путем повторной выборки, при шелчке */}
+        <input type="button" value="Refetch" onClick={() => refetch()} />
       </form>
       <BusinessResults businesses={data.businesses} />
     </div>
